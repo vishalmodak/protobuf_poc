a6174fc147b43f687173b9c38f03206d6af5beca
Vishal <vmodak@enova.com>
2019-01-30 15:53:15 -0600

    svc-loan: added statsd instrumentation on APIs

--- svc-loan/src/main/java/com/lending/loan/SvcLoanApplication.java
+++ svc-loan/src/main/java/com/lending/loan/SvcLoanApplication.java
@@ -2,8 +2,10 @@ package com.lending.loan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SvcLoanApplication {
@@ -16,6 +18,11 @@ public class SvcLoanApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SvcLoanApplication.class, args);
 	}
-
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	    builder.additionalMessageConverters(new ProtobufHttpMessageConverter());
+	   return builder.build();
+	}
 }
 
--- svc-loan/src/main/java/com/lending/loan/controllers/LoanController.java
+++ svc-loan/src/main/java/com/lending/loan/controllers/LoanController.java
@@ -5,6 +5,8 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,8 +18,10 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -27,6 +31,8 @@ import com.lending.loan.kafka.LoanRepository;
 import com.lending.proto.Loan;
 import com.lending.proto.PaymentList;
 
+import io.micrometer.core.annotation.Timed;
+
 @RestController
 public class LoanController {
 
@@ -34,15 +40,22 @@ public class LoanController {
 
     @Autowired
     private LoanRepository loanRepository;
+    
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Value("${service.payments.uri}")
     private String paymentServiceURI;
+    
+    private static MediaType protobufMediaType = MediaType.valueOf("application/x-protobuf");
+    private static MediaType jsonMediaType = MediaType.valueOf("application/json");
 
     @RequestMapping(value="/loan", method=RequestMethod.GET, produces={"text/plain"})
     public String index(@PathVariable String loanNumber) {
         return "Welcome to Loan Service...!!";
     }
 
+    @Timed(percentiles = {0.90, 0.95, 0.99}, histogram = true)
     @RequestMapping(value="/loan/{loanNumber}", method=RequestMethod.GET, produces={"application/x-protobuf", "application/json"})
     @ResponseBody
     public Loan lookupLoan(@PathVariable String loanNumber) {
@@ -58,22 +71,29 @@ public class LoanController {
         return loan;
     }
 
+    @Timed(percentiles = {0.90, 0.95, 0.99}, histogram = true)
     @RequestMapping(value="/loan/payments/{loanNumber}", method=RequestMethod.GET, produces={"application/json","application/x-protobuf"})
-    public PaymentList lookupPayments(@PathVariable String loanNumber) {
+    public PaymentList lookupPayments(@PathVariable String loanNumber, @RequestParam(name = "dup", required = false) Integer dupCount, 
+            @RequestHeader HttpHeaders headers) {
         String getPaymentsURI = paymentServiceURI + "/v1/payments/{loanNumber}";
+        if (dupCount != null && dupCount > 1) {
+            getPaymentsURI += "?dup=" + dupCount;
+        }
 
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new ProtobufHttpMessageConverter());
+        HttpHeaders outheaders = new HttpHeaders();
+        if (headers.getAccept().contains(protobufMediaType)) {
+            outheaders.setAccept(Arrays.asList(protobufMediaType));
+        } else if (headers.getAccept().contains(jsonMediaType)) {
+            outheaders.setAccept(Arrays.asList(jsonMediaType));
+        }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList((MediaType.valueOf("application/x-protobuf"))));
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+        final HttpEntity<String> entity = new HttpEntity<String>(outheaders);
 
         Map<String, String> params = new HashMap<>();
         params.put("loanNumber", loanNumber);
 
         ResponseEntity<PaymentList> response = restTemplate.exchange(getPaymentsURI, HttpMethod.GET, entity, PaymentList.class, params);
-        log.info("Loan: " + response.getBody());
+//        log.info("Loan: " + response.getBody());
 
         return response.getBody();
     }
